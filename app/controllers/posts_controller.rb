1754b881bde5537dd326378dbf88c516821d662d
Saint-Jean Germila <germila@noukod.com>
2019-04-17 14:38:44 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,11 +8,8 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
-      #BEFORE using a serializer:
-      render json: @post.to_json(only: [:title, :description, :id],
-                                include: [author: { only: [:name]}])
-       # AFTER USING OUR SERIALIZER
-      # render json: @post, status: 200
+    render json: @post.to_json(only: [:title, :description, :id],
+                              include: [author: { only: [:name]}])
   end
 
   def new
@@ -44,3 +41,47 @@ private
     params.require(:post).permit(:title, :description)
   end
 end
+© 2019 GitHub, Inc.class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update]
+
+  def index
+    @posts = Post.all
+    render json: @posts, status: 200
+  end
+
+  def show
+    @post = Post.find(params[:id])
+    render json: @post.to_json(only: [:title, :description, :id],
+                              include: [author: { only: [:name]}])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.create(post_params)
+    @post.save
+    render json: @post, status: 201
+  end
+
+  def edit
+  end
+
+  def update
+    @post.update(post_params)
+    render json: @post, status: 202
+  end
+
+private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def post_params
+    params.require(:post).permit(:title, :description)
+  end
+end
+© 2019 GitHub, Inc.
